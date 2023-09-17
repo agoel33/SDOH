7d06ba96e1aa49b3545fcfd282f2c2e2b2f8f3a2
agoel33 <akulgoel6@gmail.com>
2023-09-17 21:30:13 +0000

    hi

--- bart_model.py
+++ bart_model.py
@@ -9,22 +9,27 @@ from nltk.tokenize import sent_tokenize
 file_path = '../discharge_with_social_final.csv'
 df = pd.read_csv(file_path)
 
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli",
-                      device_map= "auto")
+models = ['MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli', 'sileod/deberta-v3-base-tasksource-nli', "MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli",
+          "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7", "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", "joeddav/xlm-roberta-large-xnli "
+          "optimum/distilbert-base-uncased-mnli"]
+for mdl in model:
+  classifier = pipeline("zero-shot-classification",
+                        model= mdl,
+                        device_map= "auto")
 
-bart_list = []
-for i in range(50):
-  print(i)
-  entry = df.loc[i]['text']
-  sentences = sent_tokenize(entry)
-  for sentence in sentences:
-    sequence_to_classify = f"NOTE {i} " + sentence
-    candidate_labels = ['relative needing care', 'no relative needing care', 'not specified']
-    bart_list.append(classifier(sequence_to_classify, candidate_labels))
+  model_classification = []
+  for i in range(5):
+    print(i)
+    entry = df.loc[i]['text']
+    sentences = sent_tokenize(entry)
+    for sentence in sentences:
+      sequence_to_classify = f"NOTE {i} " + sentence
+      candidate_labels = ['relative needing care', 'no relative needing care', 'not specified']
+      model_classification.append(classifier(sequence_to_classify, candidate_labels))
 
-output_file_path = 'C:/Users/Ajay_XPS_9380/desktop/bart_relative_care.json'
+  output_file_path = f'../SDOH/{mdl}_relative_care.json'
 
-with open(output_file_path, 'w') as fout:
-  json.dump(bart_list, fout)
+  with open(output_file_path, 'w') as fout:
+    json.dump(model_classification, fout)
+  print(f"{mdl} is done")
 
