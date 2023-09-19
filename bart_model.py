b69474ef63b5316ca1bed0664d2fe9129c384bd1
agoel33 <103295535+agoel33@users.noreply.github.com>
2023-09-18 17:22:18 -0700

    Update bart_model.py

--- bart_model.py
+++ bart_model.py
@@ -11,13 +11,16 @@ df = pd.read_csv(file_path)
 
 does_not_support_gpu = ['MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli',  'sileod/deberta-v3-base-tasksource-nli', "MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli",
           "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7", "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", "NDugar/v2xl-again-mnli", "NDugar/1epochv3"]
-
 permission_error = [ "joeddav/xlm-roberta-large-xnli"]
-
 token_too_long = ["DAMO-NLP-SG/zero-shot-classify-SSTuning-base"]
-possible_labels = [["homeless", "not specified", "not homeless"], ["food insecure", "not specified", "not food insecure"], ["low income", "not specified", "not low income"], 
-["marital estrangement", "not specified", "no marital estrangement"]]
-models = ["valhalla/distilbart-mnli-12-1", "valhalla/distilbart-mnli-12-9", "HiTZ/A2T_RoBERTa_SMFA_WikiEvents-arg_ACE-arg"]
+
+models_used = ["valhalla/distilbart-mnli-12-1", "valhalla/distilbart-mnli-12-9", "HiTZ/A2T_RoBERTa_SMFA_WikiEvents-arg_ACE-arg"]
+
+possible_labels = [['relative needing care', 'no relative needing care', 'not specified'], ["employed", "not specified", "unemployed"], 
+["inprisonment or other incarceration", "not specified", "no impisonment or other incarceration"]["homeless", "not specified", "not homeless"], 
+["food insecure", "not specified", "not food insecure"], ["low income", "not specified", "not low income"], ["marital estrangement", "not specified", "no marital estrangement"]]
+
+models = ["eleldar/theme-classification", "Narsil/bart-large-mnli-opti", "morit/english_xlm_xnli", "BSC-LT/sciroshot"]
 
 for label in possible_labels:
   print(f"{label}")
